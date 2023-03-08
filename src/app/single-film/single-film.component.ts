8bdc9ef8cf4a60aea77a04d145de83ea9f1fa06f
KevinWissmer <kevin.wissmer@mail.de>
2023-03-08 14:53:18 +0100

    added ATInput functionality

--- src/app/single-film/single-film.component.ts
+++ src/app/single-film/single-film.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-film',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-film.component.scss']
 })
 export class SingleFilmComponent {
+  @Input() filmName: string = '';
+  @Input('singleFilmName') filmNameSecondApproach: string = '';
+  notstatus: string = 'viewed';
+  status: string = 'not viewed';
+
+
+  setStatus() {
+    let tmp = this.status;
+    this.status = this.notstatus;
+    this.notstatus = tmp;
+  }
+
+
+
+
+
+
+
+
 
 }
+
